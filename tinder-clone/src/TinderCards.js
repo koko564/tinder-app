f1225e8d506331ec526b197abad6821d776e4b30
Koena Das <daskoena96@gmail.com>
2022-02-13 15:40:12 +0530

    backendd

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './TinderCards.css';
 import TinderCard from 'react-tinder-card';
+import axios from './axios';
 
 function TinderCards() {
-    const [people, setPeople]=useState([
-        {
-            name:'Dora Jones',
-            url:'https://discoverymood.com/wp-content/uploads/2020/04/Mental-Strong-Women-min-480x340.jpg',
+    const [people, setPeople]=useState([]);
 
-        },
-        {
-            name:'Tina Ray',
-            url:'https://www.simplemost.com/wp-content/uploads/2017/04/532017720_priyanka-chopra.jpg',
+    useEffect(()=>{
+        async function fetchData(){
+            const req=await axios.get('/tinder/cards')
 
+            setPeople(req.data);
         }
-    ]);
+        fetchData();
+    }, [])
+
+    console.log(people)
+
     const swiped=(direction,nameToDelete)=>{
         console.log("removing: "+nameToDelete);
         //setLastDirection(direction);
@@ -33,7 +35,7 @@ function TinderCards() {
         onSwipe={(dir)=>swiped(dir,person.name)}
         onCardLeftScreen={()=>outOfFrame(person.name)}>
             <div
-            style={{backgroundImage: `url(${person.url})`}}
+            style={{backgroundImage: `url(${person.imgUrl})`}}
             className='card'
             >
                 <h3>{person.name}</h3>
